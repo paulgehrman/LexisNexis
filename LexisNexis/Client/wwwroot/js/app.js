0fc46b77719c3794bcbc6f23637ae3b27492e1b0
paul <paulgehrman@hotmail.com>
2024-04-09 15:14:01 -0700

    commit

---
+++ LexisNexis/Client/wwwroot/js/app.js
@@ -0,0 +1,15 @@
+function showAlert() {
+    alert("hello");
+}
+
+function displayBorder() {
+    $("#accordion").css("border", "3px solid red");
+}
+
+function slide() {
+    $("#accordion").slideDown(2000);
+}
+
+function makeBold() {
+    $("#accordion").css("font-weight", "bold");
+}
