bd6fa787e3233b729c038cb81ad1fc650ff7bd52
paul <paulgehrman@hotmail.com>
2024-04-10 15:00:19 -0700

    commit

---
+++ LexisNexis/Shared/Utility.cs
@@ -0,0 +1,47 @@
+﻿namespace LexisNexis.Shared
+{
+    public static class Utility
+    {
+        /*****************************************************
+        * Get Mime Type
+       *****************************************************/
+        public static string GetMimeType(string fileName)
+        {
+            string mimeType = String.Empty;
+
+            //get file extension
+            string fileExtension = GetFileExtension(fileName);
+
+            switch (fileExtension.ToLower())
+            {
+                case "jpg":
+                case "jpeg":
+                case "jfif":
+                    mimeType = "image/jpeg";
+                    break;
+
+                case "gif":
+                    mimeType = "image/gif";
+                    break;
+
+                case "bmp":
+                    mimeType = "image/bmp";
+                    break;
+
+                case "png":
+                    mimeType = "image/png";
+                    break;
+            }
+            return mimeType;
+        }
+
+        /*****************************************************
+         * Get File Extension
+        *****************************************************/
+        public static string GetFileExtension(string fileName)
+        {
+            string[] splitString = fileName.Split(".");
+            return splitString[splitString.Length - 1];
+        }
+    }
+}
